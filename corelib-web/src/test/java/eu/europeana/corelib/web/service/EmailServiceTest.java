10a3fad425f9e08fcd94dcb8ece92b6d60b0e96c
Willem-Jan.Boogerd@kb.nl <Willem-Jan.Boogerd@kb.nl>
2012-01-16 16:24:08 +0000

    CORELIB-WEB: Get the email service working against the simple test, more tests needed for full code coverage...

--- corelib-web/src/test/java/eu/europeana/corelib/web/service/EmailServiceTest.java
+++ corelib-web/src/test/java/eu/europeana/corelib/web/service/EmailServiceTest.java
@@ -1,8 +1,11 @@
 package eu.europeana.corelib.web.service;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,13 +18,19 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.subethamail.wiser.Wiser;
 
+import eu.europeana.corelib.definitions.db.entity.Token;
+import eu.europeana.corelib.web.exception.EmailServiceException;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/corelib-web-context.xml", "/corelib-web-test.xml" })
 public class EmailServiceTest extends AbstractJUnit4SpringContextTests {
 	
 	public static int SERVER_PORT = 2500;
 
-    private Wiser wiser;
+    private static Wiser wiser;
+    
+    @Resource
+    private EmailService emailService;
     
 	@Before
 	public void setUp() throws Exception {
@@ -36,10 +45,46 @@ public class EmailServiceTest extends AbstractJUnit4SpringContextTests {
 	public void tearDown() throws Exception {
 		wiser.stop();
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSendToken() throws EmailServiceException {
+		Token token = createFakeToken("testSendToken","testSendToken@testSendToken.eu");
+		emailService.sendToken(token, "testSendToken.eu/test.html");
+		assertEquals(1, wiser.getMessages().size());
+	}
+
+	@SuppressWarnings("serial")
+	private Token createFakeToken(final String tokenString, final String emailAddress) {
+		return new Token() {
+			@Override
+			public String getId() {
+				return tokenString;
+			}
+			
+			@Override
+			public void setToken(String token) {}
+			
+			@Override
+			public void setEmail(String email) {}
+			
+			@Override
+			public void setCreated(Date created) {}
+			
+			@Override
+			public String getToken() {
+				return getId();
+			}
+			
+			@Override
+			public String getEmail() {
+				return emailAddress;
+			}
+			
+			@Override
+			public Date getCreated() {
+				return new Date();
+			}
+		};
 	}
 	
     private static void reconfigureMailSenders(ApplicationContext applicationContext, int port) {
