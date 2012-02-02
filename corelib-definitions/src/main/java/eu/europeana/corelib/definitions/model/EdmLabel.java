038ea65e80bd795b195cf12c1861f4ab20fb2037
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2012-02-02 16:49:12 +0000

    CORELIB-SOLR Refactored code and added the  missing Proxy Implementation - Changed License

--- corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/EdmLabel.java
+++ corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/EdmLabel.java
@@ -1,5 +1,26 @@
-package eu.europeana.corelib.definitions.model;
+/*
+ * Copyright 2007-2012 The Europeana Foundation
+ *
+ *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
+ *  by the European Commission;
+ *  You may not use this work except in compliance with the Licence.
+ * 
+ *  You may obtain a copy of the Licence at:
+ *  http://joinup.ec.europa.eu/software/page/eupl
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of
+ *  any kind, either express or implied.
+ *  See the Licence for the specific language governing permissions and limitations under
+ *  the Licence.
+ */
 
+package eu.europeana.corelib.definitions.model;
+/**
+ * TO BE UPDATED
+ * @author Yorgos.Mamakis@ kb.nl
+ *
+ */
 public enum EdmLabel {
 	TIMESTAMP("timestamp"), UID("uid"), EDM_TYPE("edm_type"), ORE_AGGREGATION(
 			"ore_aggregation"), EDM_AGGREGATION("edm_aggregation"), EDM_AGGREGATED_CHO(
