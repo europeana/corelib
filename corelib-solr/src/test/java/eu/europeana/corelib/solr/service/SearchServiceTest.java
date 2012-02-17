74b03b81e507aba7ffae2ac9fff662927aff9a57
Willem-Jan.Boogerd@kb.nl <Willem-Jan.Boogerd@kb.nl>
2012-02-17 15:35:44 +0000

    CORELIB: reorganising SOLR module tests

---
+++ corelib-solr/src/test/java/eu/europeana/corelib/solr/service/SearchServiceTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2007-2012 The Europeana Foundation
+ *
+ *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
+ *  by the European Commission;
+ *  You may not use this work except in compliance with the Licence.
+ * 
+ *  You may obtain a copy of the Licence at:
+ *  http://joinup.ec.europa.eu/software/page/eupl
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of
+ *  any kind, either express or implied.
+ *  See the Licence for the specific language governing permissions and limitations under
+ *  the Licence.
+ */
+
+package eu.europeana.corelib.solr.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Willem-Jan Boogerd <www.eledge.net/contact>
+ * 
+ * @see eu.europeana.corelib.solr.service.SearchService
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/corelib-solr-context.xml", "/corelib-solr-test.xml" })
+public class SearchServiceTest {
+	
+	@Test
+	public void notest() {
+		// TODO
+	}
+
+
+}
