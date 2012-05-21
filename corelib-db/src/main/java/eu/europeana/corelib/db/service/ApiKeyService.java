d179f4958c562d3a8d4c1bcc92b3460d46be745f
Willem-Jan.Boogerd@kb.nl <Willem-Jan.Boogerd@kb.nl>
2012-05-21 21:34:13 +0000

    CORELIB_DB: added direct service to check api2 keys

---
+++ corelib-db/src/main/java/eu/europeana/corelib/db/service/ApiKeyService.java
@@ -0,0 +1,8 @@
+package eu.europeana.corelib.db.service;
+
+import eu.europeana.corelib.db.service.abstracts.AbstractService;
+import eu.europeana.corelib.definitions.db.entity.relational.ApiKey;
+
+public interface ApiKeyService extends AbstractService<ApiKey> {
+
+}
---
+++ corelib-db/src/main/java/eu/europeana/corelib/db/service/impl/ApiKeyServiceImpl.java
@@ -0,0 +1,10 @@
+package eu.europeana.corelib.db.service.impl;
+
+import eu.europeana.corelib.db.service.ApiKeyService;
+import eu.europeana.corelib.db.service.abstracts.AbstractServiceImpl;
+import eu.europeana.corelib.definitions.db.entity.relational.ApiKey;
+
+public class ApiKeyServiceImpl extends AbstractServiceImpl<ApiKey>  implements ApiKeyService {
+
+
+}
