5ec5e14eed53f56559aaeb5c50fdeded161eb563
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2012-06-19 15:14:36 +0000

    Fixe in return

--- corelib-solr-dereference/src/main/java/eu/europeana/corelib/dereference/impl/EuropeanaField.java
+++ corelib-solr-dereference/src/main/java/eu/europeana/corelib/dereference/impl/EuropeanaField.java
@@ -23,17 +23,17 @@ public class EuropeanaField {
 	private UriField uriField;
 	enum UriField {
 
-		EDM_DATAPROVIDER("edm:dataProvider"), EDM_PROVIDER("edm:provider"), DC_CREATOR(
-				"dc:creator"), DC_CONTRIBUTOR("dc:contributor"), DC_DATE(
-				"dc:date"), DC_DESCRIPTION("dc:description"), DC_PUBLISHER(
-				"dc:publisher"), DC_RELATION("dc:relation"), DC_SUBJECT(
-				"dc:subject"), DC_TYPE("dc:type"), DCTERMS_CREATED(
-				"dcterms:created"), DCTERMS_EXTENT("dcterms:extent"), DCTERMS_ISSUED(
-				"dcterms:issued"), DCTERMS_PROVENANCE("dcterms:provenance"), DCTERMS_REPLACES(
-				"dcterms:replaces"), DCTERMS_SPATIAL("dcterms:spatial"), DCTERMS_TABLEOFCONTENTS(
-				"dcterms:tableOfContents"), DCTERMS_TEMPORAL("dcterms:temporal"), EDM_BROADER(
-				"edm:broader"), EDM_CURRENTLOCATION("edm:currentLocation"), DCTERMS_ISPARTOF(
-				"dcterms:isPartOf");
+		EDM_DATAPROVIDER("provider_aggregation_edm_dataProvider"), EDM_PROVIDER("provider_aggregation_edm_provider"), DC_CREATOR(
+				"provider_dc_creator"), DC_CONTRIBUTOR("provider_dc_contributor"), DC_DATE(
+				"provider_dc_date"), DC_DESCRIPTION("provider_dc_description"), DC_PUBLISHER(
+				"provider_dc_publisher"), DC_RELATION("provider_dc_relation"), DC_SUBJECT(
+				"provider_dc_subject"), DC_TYPE("provider_dc_type"), DCTERMS_CREATED(
+				"provider_dcterms_created"), DCTERMS_EXTENT("provider_dcterms_extent"), DCTERMS_ISSUED(
+				"provider_dcterms_issued"), DCTERMS_PROVENANCE("provider_dcterms_provenance"), DCTERMS_REPLACES(
+				"provider_dcterms_replaces"), DCTERMS_SPATIAL("provider_dcterms_spatial"), DCTERMS_TABLEOFCONTENTS(
+				"provider_dcterms_tableOfContents"), DCTERMS_TEMPORAL("provider_dcterms_temporal"), SKOS_BROADER(
+				"cc_skos_broader"), EDM_CURRENTLOCATION_LAT("provider_edm_currentLocation_lat"), DCTERMS_ISPARTOF(
+				"provider_dcterms_isPartOf");
 
 		private String uriField;
 
@@ -71,7 +71,9 @@ public class EuropeanaField {
 	
 	public static boolean contains (String field){
 		for(UriField uriField: UriField.values()){
-			if (StringUtils.equals(uriField.toString(), field));
+			if (StringUtils.equals(uriField.toString(), field)){
+				return true;
+			}
 		}
 		return false;
 	}
