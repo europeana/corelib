aea675ee931cf0bf14b8e1dca6bc07228ab1fa97
d_entous@yahoo.com <d_entous@yahoo.com>
2012-07-01 13:36:41 +0000

    solr-definitions after mvn clean build

--- corelib-solr-definitions/src/main/java/eu/europeana/corelib/definitions/jibx/UGCType.java
+++ corelib-solr-definitions/src/main/java/eu/europeana/corelib/definitions/jibx/UGCType.java
@@ -6,11 +6,29 @@ package eu.europeana.corelib.definitions.jibx;
  * <pre>
  * &lt;xs:simpleType xmlns:ns="http://www.europeana.eu/schemas/edm/" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="UGCType">
  *   &lt;xs:restriction base="xs:string">
- *     &lt;xs:enumeration value="TRUE"/>
+ *     &lt;xs:enumeration value="true"/>
  *   &lt;/xs:restriction>
  * &lt;/xs:simpleType>
  * </pre>
  */
 public enum UGCType {
-    TRUE
+    TRUE("true");
+    private final String value;
+
+    private UGCType(String value) {
+        this.value = value;
+    }
+
+    public String xmlValue() {
+        return value;
+    }
+
+    public static UGCType convert(String value) {
+        for (UGCType inst : values()) {
+            if (inst.xmlValue().equals(value)) {
+                return inst;
+            }
+        }
+        return null;
+    }
 }
