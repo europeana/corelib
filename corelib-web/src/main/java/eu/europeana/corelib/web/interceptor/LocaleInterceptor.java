211762e0843aedd888bd93af838c00dfddc9de73
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-04 13:16:55 +0000

    Partial fix for language switch (fails on fulldoc)

---
+++ corelib-web/src/main/java/eu/europeana/corelib/web/interceptor/LocaleInterceptor.java
@@ -0,0 +1,54 @@
+package eu.europeana.corelib.web.interceptor;
+
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.propertyeditors.LocaleEditor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import eu.europeana.corelib.definitions.exception.EuropeanaException;
+
+										
+public class LocaleInterceptor  extends HandlerInterceptorAdapter{
+	
+	private static Logger log = Logger.getLogger(LocaleInterceptor.class.getName());
+	public static final String DEFAULT_PARAM_NAME = "locale";
+	private String paramName = DEFAULT_PARAM_NAME;
+	
+    public void setParamName(String paramName) {
+        this.paramName = paramName;
+    }
+
+    public String getParamName() {
+        return this.paramName;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)  throws IllegalStateException{
+    	
+    	String newLocale = request.getParameter(this.paramName);
+    	
+    	log.info("LocaleInterceptor.preHandle gets parameter '" + this.paramName + "' with value " + newLocale);
+        
+        if(newLocale != null && !newLocale.equals("") && !(newLocale.contains("*"))){
+        	
+            LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+            if (localeResolver == null) {
+                throw new IllegalStateException("No LocaleResolver found: not in a DispatcherServlet request?");
+            }
+            LocaleEditor localeEditor = new LocaleEditor();
+            localeEditor.setAsText(newLocale);
+            localeResolver.setLocale(request, response, (Locale)localeEditor.getValue());
+        }
+        return true;
+    }
+}
+
+
+
