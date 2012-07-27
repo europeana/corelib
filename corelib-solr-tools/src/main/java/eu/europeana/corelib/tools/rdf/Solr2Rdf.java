51924817b3ca40b80ba374cc3c1a2afc7fa672fb
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2012-07-27 15:09:17 +0000

    Unit tests and XSLT for XSL transformation from SolrDocument to JiBX object

---
+++ corelib-solr-tools/src/main/java/eu/europeana/corelib/tools/rdf/Solr2Rdf.java
@@ -0,0 +1,130 @@
+package eu.europeana.corelib.tools.rdf;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.jibx.runtime.BindingDirectory;
+import org.jibx.runtime.IBindingFactory;
+import org.jibx.runtime.IUnmarshallingContext;
+import org.jibx.runtime.JiBXException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import eu.europeana.corelib.definitions.jibx.RDF;
+
+public class Solr2Rdf {
+	ByteArrayOutputStream out;
+	StreamResult streamResult;
+	ContentHandler hd;
+	public void initialize() throws TransformerConfigurationException,
+			SAXException, IOException {
+		out = new ByteArrayOutputStream();
+		OutputFormat of = new OutputFormat("XML","UTF-8",true);
+		of.setIndent(1);
+		of.setIndenting(true);
+		XMLSerializer serializer = new XMLSerializer(out,of);
+		hd = serializer.asContentHandler();
+		hd.startDocument();
+		AttributesImpl atts = new AttributesImpl();
+		hd.startElement("", "", "record", atts);
+	}
+
+	public RDF constructFromMap(SolrInputDocument solrDocument,
+			Map<String, List<String>> map) throws SAXException, IOException,
+			TransformerException, JiBXException {
+
+		createXMLFromSolr(solrDocument);
+		for (Entry<String, List<String>> mapField : map.entrySet()) {
+			for (String str : mapField.getValue()) {
+				createXMLElement(mapField.getKey(), (String) str);
+			}
+		}
+		serializeDoc();
+		return transformDoc();
+	}
+
+	private RDF transformDoc() throws TransformerException, JiBXException, IOException {
+		String output = out.toString("UTF-8");
+		System.out.println("Output:" + output);
+		ByteArrayOutputStream transformedOutput = new ByteArrayOutputStream();
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+		Transformer transform = tFactory.newTransformer(new StreamSource("src/main/resources/convertToEdm.xsl"));
+		transform.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		transform.setOutputProperty(OutputKeys.INDENT, "yes");
+		transform.transform(new SAXSource(new InputSource(new ByteArrayInputStream(output.getBytes()))),new StreamResult(transformedOutput));
+		transformedOutput.flush();
+		String edmString = transformedOutput.toString("UTF-8");
+		System.out.println("Edm:" + edmString);
+		transformedOutput.close();
+		IBindingFactory bfact = BindingDirectory.getFactory(RDF.class);
+
+		IUnmarshallingContext uctx = bfact.createUnmarshallingContext();
+		return (RDF) uctx.unmarshalDocument(new StringReader(edmString));
+	}
+
+	private void serializeDoc() throws SAXException, IOException {
+		hd.endElement("", "", "record");
+		
+		//out.flush();
+	}
+
+	public RDF constructFromSolrDocument(SolrInputDocument solrDocument)
+			throws SAXException, IOException, TransformerException, JiBXException {
+
+		createXMLFromSolr(solrDocument);
+		serializeDoc();
+		return transformDoc();
+	}
+
+	private TransformerHandler createDocument()
+			throws TransformerConfigurationException {
+		SAXTransformerFactory tf = (SAXTransformerFactory) SAXTransformerFactory
+				.newInstance();
+		return tf.newTransformerHandler();
+	}
+
+	private void createXMLFromSolr(SolrInputDocument solrDocument)
+			throws SAXException {
+		for (Entry<String, SolrInputField> solrField : solrDocument.entrySet()) {
+			for (Object str : solrField.getValue().getValues().toArray()) {
+				if(str instanceof String){
+					createXMLElement(solrField.getKey(), (String) str);
+				}
+				else if (str instanceof Boolean){
+					createXMLElement(solrField.getKey(),Boolean.toString((Boolean)str));
+				}
+			}
+		}
+	}
+
+	private void createXMLElement(String name, String value)
+			throws SAXException {
+		AttributesImpl atts = new AttributesImpl();
+		hd.startElement("", "", name, atts);
+		hd.characters(value.toCharArray(), 0, value.length());
+		hd.endElement("", "", "USER");
+		
+	}
+}
