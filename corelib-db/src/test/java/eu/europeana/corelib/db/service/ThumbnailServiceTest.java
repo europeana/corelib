d278af5058923d7ec401ad694680f8fb387ca6dd
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2012-09-04 11:20:36 +0000

    Updated Image storage unit tests and added LOG4j support

--- corelib-db/src/test/java/eu/europeana/corelib/db/service/ThumbnailServiceTest.java
+++ corelib-db/src/test/java/eu/europeana/corelib/db/service/ThumbnailServiceTest.java
@@ -22,23 +22,13 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import junit.framework.Assert;
+import org.apache.log4j.Logger;
 import org.apache.sanselan.ImageReadException;
-import org.apache.sanselan.ImageWriteException;
-import org.apache.sanselan.Sanselan;
-import org.apache.sanselan.common.IImageMetadata;
-import org.apache.sanselan.common.byteSources.ByteSourceFile;
-import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
-import org.apache.sanselan.formats.jpeg.JpegPhotoshopMetadata;
-import org.apache.sanselan.formats.jpeg.exifRewrite.ExifRewriter;
-import org.apache.sanselan.formats.jpeg.xmp.JpegXmpRewriter;
-import org.apache.sanselan.formats.tiff.TiffImageMetadata;
-import org.apache.sanselan.formats.tiff.write.TiffOutputSet;
 import org.jibx.runtime.BindingDirectory;
 import org.jibx.runtime.IBindingFactory;
 import org.jibx.runtime.IUnmarshallingContext;
@@ -48,7 +38,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import eu.europeana.corelib.db.dao.NosqlDao;
 import eu.europeana.corelib.db.entity.nosql.Image;
 import eu.europeana.corelib.db.entity.nosql.ImageCache;
@@ -58,83 +47,132 @@ import eu.europeana.corelib.definitions.model.ThumbSize;
 
 /**
  * @author Willem-Jan Boogerd <www.eledge.net/contact>
+ * @author Georgios Markakis <gwarkx@hotmail.com>
+ * 
+ * @since 4 May 2012
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/corelib-db-context.xml", "/corelib-db-test.xml" })
 public class ThumbnailServiceTest {
 
-	@Resource(name="corelib_db_imageDao")
-	NosqlDao<ImageCache, String> imageDao;
+	private static Logger LOGGER = Logger.getLogger(ThumbnailServiceTest.class);
 	
+	@Resource(name = "corelib_db_imageDao")
+	NosqlDao<ImageCache, String> imageDao;
+
 	@Resource
 	ThumbnailService thumbnailService;
-	
+
 	BufferedImage image;
-	
+
 	final String OBJ_ID = "objectTest";
 	final String COL_ID = "collectionTest";
-	
+
+	/**
+	 * Initialise the testing session
+	 * 
+	 * @throws IOException
+	 */
 	@Before
 	public void setup() throws IOException {
 		imageDao.getCollection().drop();
-		image = ImageIO.read( getClass().getResourceAsStream("/images/GREATWAR.jpg") );
+		image = ImageIO.read(getClass().getResourceAsStream(
+				"/images/GREATWAR.jpg"));
 	}
-	
-	@Test
-	public void storeTest() throws IOException, DatabaseException, ImageReadException {
 
-		
+	/**
+	 * Image storage Test (no XMP information)
+	 * 
+	 * @throws IOException
+	 * @throws DatabaseException
+	 * @throws ImageReadException
+	 */
+	@Test
+	public void storeTest() throws IOException, DatabaseException,
+			ImageReadException {
+
 		Assert.assertTrue("Schema not empty...", imageDao.count() == 0);
-        
-		ImageCache cache = thumbnailService.storeThumbnail(OBJ_ID, COL_ID, image, "/images/GREATWAR.jpg");
-		
-		Assert.assertTrue("Test item does not exist in MongoDB", thumbnailService.exists(OBJ_ID));
-		
-		byte[] tiny = thumbnailService.retrieveThumbnail(OBJ_ID, ThumbSize.TINY);
-		
-		Assert.assertTrue(tiny.length == cache.getImages().get(ThumbSize.TINY.toString()).getImage().length );
-		
+
+		ImageCache cache = thumbnailService.storeThumbnail(OBJ_ID, COL_ID,
+				image, "/images/GREATWAR.jpg");
+
+		Assert.assertTrue("Test item does not exist in MongoDB",
+				thumbnailService.exists(OBJ_ID));
+
+		byte[] tiny = thumbnailService
+				.retrieveThumbnail(OBJ_ID, ThumbSize.TINY);
+
+		Assert.assertTrue(tiny.length == cache.getImages()
+				.get(ThumbSize.TINY.toString()).getImage().length);
+
 		ImageCache imageCache = thumbnailService.findByID(OBJ_ID);
-		
+
 		Image tinyImage = imageCache.getImages().get(ThumbSize.TINY.toString());
-		
+
 		Assert.assertTrue(tinyImage.getHeight() > 0);
 		Assert.assertEquals(tinyImage.getWidth(), ThumbSize.TINY.getMaxWidth());
-		
-		Image largeImage = imageCache.getImages().get(ThumbSize.LARGE.toString());
+
+		Image largeImage = imageCache.getImages().get(
+				ThumbSize.LARGE.toString());
 		InputStream in = new ByteArrayInputStream(largeImage.getImage());
-		
+
 	}
-	
+
 	/**
+	 * Image Storage Test (XMP Information added)
+	 * 
 	 * @throws JiBXException
 	 * @throws FileNotFoundException
 	 * @throws DatabaseException
 	 */
 	@Test
-	public void storewithXMPTest() throws JiBXException, FileNotFoundException, DatabaseException {
-		
+	public void storewithXMPTest() throws JiBXException, FileNotFoundException,
+			DatabaseException {
+
+		// Create an EDM object representation from a sample EDM XML file
 		IBindingFactory context = BindingDirectory.getFactory(RDF.class);
-		
+
 		IUnmarshallingContext uctx = context.createUnmarshallingContext();
-		
+
 		File test = new File("src/test/resources/edm/edmsample.xml");
-		
+
 		InputStream ins = new FileInputStream(test);
-		
+
 		RDF edm = (RDF) uctx.unmarshalDocument(ins, "UTF-8");
-		
-		ImageCache cache = thumbnailService.storeThumbnail(OBJ_ID, COL_ID, image, "/images/GREATWAR.jpg",edm);
-		
-		
+
+		//Store the image by passing the EDM object as a parameter. All
+		//relevant information contained in the EDM object will be embedded
+		//in the file in the form of XMP (RDF) data
+		ImageCache cache = thumbnailService.storeThumbnail(OBJ_ID, COL_ID,
+				image, "/images/GREATWAR.jpg", edm);
+
 		ImageCache imageCache = thumbnailService.findByID(OBJ_ID);
-		Assert.assertNotNull(imageCache);
-		String xmpInfo = thumbnailService.extractXMPInfo(OBJ_ID, COL_ID, ThumbSize.TINY);
+		Assert.assertNotNull("Image was not stored successfully in MongoDB",
+				imageCache);
 		
+		//Check info for TINY thumbnail
+		String xmpInfoTiny = thumbnailService.extractXMPInfo(OBJ_ID, COL_ID,
+				ThumbSize.TINY);
+		Assert.assertNotNull("Metadata for TINY image not stored successfully",
+				xmpInfoTiny);
+		LOGGER.info("== TINY thumbnail XMP Metadata: ==");
+		LOGGER.info(xmpInfoTiny);
 		
+		//Check info for MEDIUM thumbnail
+		String xmpInfoMed = thumbnailService.extractXMPInfo(OBJ_ID, COL_ID,
+				ThumbSize.MEDIUM);
+		Assert.assertNotNull("Metadata for MEDIUM image not stored successfully",
+				xmpInfoMed);
+		LOGGER.info("== MEDIUM thumbnail XMP Metadata: ==");
+		LOGGER.info(xmpInfoMed);
 		
-		System.out.println(xmpInfo);
-		
+		//Check info for LARGE thumbnail
+		String xmpInfoLarge = thumbnailService.extractXMPInfo(OBJ_ID, COL_ID,
+				ThumbSize.LARGE);
+		Assert.assertNotNull("Metadata for MEDIUM image not stored successfully",
+				xmpInfoLarge);
+		LOGGER.info("== LARGE thumbnail XMP Metadata: ==");
+		LOGGER.info(xmpInfoLarge);
 	}
 
 }
