9c8d31657b88c7de5396bce7fd49cf82668f26f0
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2012-11-06 15:52:14 +0000

    Unit Tests

---
+++ corelib-solr-tools/src/test/java/eu/europeana/corelib/tools/lookuptable/EuropeanaIdMongoServerTest.java
@@ -0,0 +1,44 @@
+package eu.europeana.corelib.tools.lookuptable;
+
+import java.net.UnknownHostException;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+public class EuropeanaIdMongoServerTest {
+	EuropeanaIdMongoServer server;
+	@Test
+	public void test(){
+		try {
+			server = new EuropeanaIdMongoServer(new Mongo("localhost",27017), "europeanaId_test");
+			EuropeanaId eid = new EuropeanaId();
+			eid.setNewId("newId");
+			eid.setOldId("oldId");
+			server.saveEuropeanaId(eid);
+			
+			Assert.assertTrue(server.newIdExists("oldId"));
+			Assert.assertTrue(server.oldIdExists("newId"));
+			Assert.assertEquals( server.retrieveEuropeanaIdFromNew("newId").get(0).getOldId(),"oldId");
+			Assert.assertEquals(server.retrieveEuropeanaIdFromOld("oldId").get(0).getNewId(), "newId");
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@After
+	public void cleanup(){
+		server.getDatastore().getDB().dropDatabase();
+	}
+}
