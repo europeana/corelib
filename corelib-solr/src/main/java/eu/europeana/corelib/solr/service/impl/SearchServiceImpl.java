2740a8033b8f26dd8cdc3caa405566ec029d3c28
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-11-20 10:15:46 +0000

    Enable spellcheck

--- corelib-solr/src/main/java/eu/europeana/corelib/solr/service/impl/SearchServiceImpl.java
+++ corelib-solr/src/main/java/eu/europeana/corelib/solr/service/impl/SearchServiceImpl.java
@@ -227,14 +227,14 @@ public class SearchServiceImpl implements SearchService {
 				solrQuery.setSortField("score", ORDER.desc);
 
 				// enable spellcheck
-//				if (solrQuery.getStart() == null
-//						|| solrQuery.getStart().intValue() <= 1) {
-//					solrQuery.setParam("spellcheck", "on");
-//					solrQuery.setParam("spellcheck.collate", "true");
-//					solrQuery.setParam("spellcheck.extendedResults", "true");
-//					solrQuery.setParam("spellcheck.onlyMorePopular", "true");
-//					solrQuery.setParam("spellcheck.q", query.getQuery());
-//				}
+				if (solrQuery.getStart() == null
+						|| solrQuery.getStart().intValue() <= 1) {
+					solrQuery.setParam("spellcheck", "on");
+					solrQuery.setParam("spellcheck.collate", "true");
+					solrQuery.setParam("spellcheck.extendedResults", "true");
+					solrQuery.setParam("spellcheck.onlyMorePopular", "true");
+					solrQuery.setParam("spellcheck.q", query.getQuery());
+				}
 
 				// add extra parameters if any
 				if (query.getParameters() != null) {
