96484c1d3b7c41fcec9f61e30780a9b552a2422b
wjboogerd@eledge.net <wjboogerd@eledge.net>
2013-04-23 09:23:20 +0000

    Code cleaning

---
+++ corelib-web/src/main/java/eu/europeana/corelib/web/utils/RequestUtils.java
@@ -0,0 +1,17 @@
+package eu.europeana.corelib.web.utils;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestUtils {
+	
+	@SuppressWarnings("unchecked")
+	public static Map<String, String[]> getParameterMap(HttpServletRequest request) {
+		if (request != null) {
+			return (Map<String, String[]>)request.getParameterMap();
+		}
+		return null;
+	}
+
+}
