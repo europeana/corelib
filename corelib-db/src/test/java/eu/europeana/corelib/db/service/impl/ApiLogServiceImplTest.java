199a5693de425a5cc16ae44856baa0eb1c334ce9
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2013-05-10 16:11:03 +0000

    Testing ApiLogServiceImpl

---
+++ corelib-db/src/test/java/eu/europeana/corelib/db/service/impl/ApiLogServiceImplTest.java
@@ -0,0 +1,31 @@
+package eu.europeana.corelib.db.service.impl;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import eu.europeana.corelib.db.service.ApiKeyService;
+import eu.europeana.corelib.db.service.ApiLogService;
+import eu.europeana.corelib.utils.DateIntervalUtils;
+import eu.europeana.corelib.utils.model.DateInterval;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/corelib-db-context.xml", "/corelib-db-test.xml"})
+public class ApiLogServiceImplTest {
+
+	@Resource private ApiLogService apiLogService;
+
+	@Test
+	public void testCountByApiKeyByInterval() {
+		String apiKey = "api2demo";
+		DateInterval interval = DateIntervalUtils.getToday();
+		long count = apiLogService.countByApiKeyByInterval(apiKey, interval);
+		assertNotNull(count);
+		assertEquals(0, count);
+	}
+}
