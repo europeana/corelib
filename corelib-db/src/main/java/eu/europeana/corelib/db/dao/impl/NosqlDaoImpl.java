f54364ee49277321408fe3a0dc66d5221ea5a47b
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2013-06-17 12:14:26 +0000

    Added replication support

--- corelib-db/src/main/java/eu/europeana/corelib/db/dao/impl/NosqlDaoImpl.java
+++ corelib-db/src/main/java/eu/europeana/corelib/db/dao/impl/NosqlDaoImpl.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.dao.BasicDAO;
+import com.mongodb.ReadPreference;
+import com.mongodb.ReadPreference.TaggedReadPreference;
 
 import eu.europeana.corelib.db.dao.NosqlDao;
 import eu.europeana.corelib.db.entity.nosql.abstracts.NoSqlEntity;
@@ -14,6 +16,7 @@ public class NosqlDaoImpl<E extends NoSqlEntity, T extends Serializable> extends
 	
 	public NosqlDaoImpl(Datastore datastore, Class<E> clazz) {
 		super(clazz, datastore);
+		datastore.getDB().slaveOk();
 		this.clazz = clazz;
 	}
 	
