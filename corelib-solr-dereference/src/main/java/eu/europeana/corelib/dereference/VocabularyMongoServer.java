e7fbd62bb0a2ac8d46a51e484347013544bc02f5
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2013-07-22 16:11:03 +0000

    Fixed Unit tests, refactored Implementation of Mongo Servers, Embedded Mongo instances

---
+++ corelib-solr-dereference/src/main/java/eu/europeana/corelib/dereference/VocabularyMongoServer.java
@@ -0,0 +1,29 @@
+package eu.europeana.corelib.dereference;
+
+import eu.europeana.corelib.dereference.impl.ControlledVocabularyImpl;
+import eu.europeana.corelib.solr.MongoServer;
+/**
+ * Vocabulary Mapping mmongo server
+ * @author Yorgos.Mamakis@ KB.nl
+ *
+ */
+public interface VocabularyMongoServer extends MongoServer{
+
+	/**
+	 * Retrieve the mappings for a specific resource. Rules are also taken into
+	 * account
+	 * 
+	 * @param field
+	 *            - the field of the controlled vocabulary to search for (the
+	 *            uri of the vocabulary is currently only supported)
+	 * @param filter
+	 *            - the value to match the field
+	 * @return
+	 */
+	public abstract ControlledVocabularyImpl getControlledVocabulary(
+			String field, String filter);
+
+	public abstract ControlledVocabularyImpl getControlledVocabularyByUri(
+			String uri, String name);
+
+}
