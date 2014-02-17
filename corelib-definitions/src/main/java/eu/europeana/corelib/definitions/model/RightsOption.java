26e4a8f82586e71ef03306d0fa3c49b08561a0bb
Andy MacLean <andyjmaclean@gmail.com>
2014-02-17 10:36:38 +0100

    See #1241

--- corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/RightsOption.java
+++ corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/RightsOption.java
@@ -86,7 +86,7 @@ public enum RightsOption {
 	 */
 	//EU_RR_F("http://www.europeana.eu/rights/rr-f/", "Free Access - Rights Reserved", "icon-copyright", false, true),
 	// See #1287
-	EU_RR_F("http://www.europeana.eu/rights/rr-f/", "Free Access - no re-use", "icon-copyright", false, true),
+	EU_RR_F("http://www.europeana.eu/rights/rr-f/", "Free access - no re-use", "icon-copyright", false, true),
 
 	// EU_RR_P("http://www.europeana.eu/rights/rr-p/", "Paid Access - Rights Reserved", "eu_paid_access.jpg", "eu_paid_access.png"), // +transparent
 	/**
@@ -94,7 +94,7 @@ public enum RightsOption {
 	 */
 	//EU_RR_P("http://www.europeana.eu/rights/rr-p/", "Paid Access - Rights Reserved", "icon-copyright", false, true),
 	// See #1241
-	EU_RR_P("http://www.europeana.eu/rights/rr-p/", "Paid Access - no re-use", "icon-copyright", false, true),
+	EU_RR_P("http://www.europeana.eu/rights/rr-p/", "Paid access - no re-use", "icon-copyright", false, true),
 
 	// EU_RR_R("http://www.europeana.eu/rights/rr-r/", "Restricted Access - Rights Reserved", "eu_restricted_access.jpg", "cc-restrictedaccess.png"), // +transparent
 	/**
