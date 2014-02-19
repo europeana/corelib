c9e1b7dd5df89765830d7a18ff0d90846f28c494
wjboogerd <wjboogerd@eledge.net>
2014-02-19 11:03:30 +0100

    Re #1202 - added language_??_t to ignore list for reporting

--- corelib-web/src/main/java/eu/europeana/corelib/web/support/ReportingReloadableMessageSource.java
+++ corelib-web/src/main/java/eu/europeana/corelib/web/support/ReportingReloadableMessageSource.java
@@ -28,19 +28,28 @@ public class ReportingReloadableMessageSource extends ReloadableResourceBundleMe
 	@Value("#{europeanaProperties['portal.server']}")
 	private String portalServer;
 	
+	// contains lowercase values of earlier reported codes
 	private static List<String> reportedBefore = new ArrayList<String>();
+
+	// static list of locale tags to ignore completely
+	private static List<String> ignoreList = new ArrayList<String>();
+	
+	static {
+		ignoreList.add("notranslate_carousel-item");
+		ignoreList.add("notranslate_featured-partner");
+		ignoreList.add("notranslate_featured-item");
+		ignoreList.add("language_");
+	}
 	
 	@Override
 	protected String getDefaultMessage(String code) {
-		log.info("MISSING LOCALE TAG:"+code);
+		boolean report = true;
+		for (String s: ignoreList) {
+			report &= StringUtils.containsIgnoreCase(code, s); 
+		}
 		
-		if(	!StringUtils.contains(code, "notranslate_carousel-item")
-			&&
-			!StringUtils.contains(code, "notranslate_featured-partner")	
-			&&
-			!StringUtils.contains(code, "notranslate_featured-item")	
-			
-			) {
+		if(	report && !reportedBefore.contains(code.toLowerCase())) {
+			log.warn("MISSING LOCALE TAG:"+code);
 			reportMissingTag(code);
 		}
 		
@@ -48,25 +57,22 @@ public class ReportingReloadableMessageSource extends ReloadableResourceBundleMe
 	}
 	
 	private void reportMissingTag(final String code) {
-		if (!reportedBefore.contains(code)) {
-			new Thread(new Runnable() {
-			    @Override
-				public void run() {
-					StringBuilder message = new StringBuilder();
-					message.append("portal.name: ").append(portalName).append("\n");
-					message.append("portal.server: ").append(portalServer).append("\n");
-					message.append("\n");
-					message.append("Missig tag:").append(code);
-					try {
-						emailService.sendException("Missing locale key:"+code, message.toString());
-					} catch (EmailServiceException e) {
-						log.info("CANT SEND EMAIL FOR MISSING LOCALE TAG:"+code);
-						// ignore
-					}
-					reportedBefore.add(code);
-			    }
-			}).start();
-		}
+		reportedBefore.add(code.toLowerCase());
+		new Thread(new Runnable() {
+		    @Override
+			public void run() {
+				StringBuilder message = new StringBuilder();
+				message.append("portal.name: ").append(portalName).append("\n");
+				message.append("portal.server: ").append(portalServer).append("\n");
+				message.append("\n");
+				message.append("Missig tag:").append(code);
+				try {
+					emailService.sendException("Missing locale key:"+code, message.toString());
+				} catch (EmailServiceException e) {
+					log.info("CANT SEND EMAIL FOR MISSING LOCALE TAG:"+code);
+				}
+		    }
+		}).start();
 	}
 
 }
