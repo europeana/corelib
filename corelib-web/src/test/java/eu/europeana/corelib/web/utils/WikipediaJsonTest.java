53328589d7b9c0c9ad14da4908e536fac5e42116
Kiraly Peter <peter.kiraly@kb.nl>
2014-04-10 14:59:22 +0200

    Re #1310 Creating Wikipedia API service.

--- corelib-web/src/test/java/eu/europeana/corelib/web/utils/WikipediaJsonTest.java
+++ corelib-web/src/test/java/eu/europeana/corelib/web/utils/WikipediaJsonTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import com.google.gson.Gson;
 
 import eu.europeana.corelib.web.model.wikipedia.WikipediaQuery;
+import eu.europeana.corelib.web.model.wikipedia.WikipediaQuery.Query.Page;
 
 public class WikipediaJsonTest {
 
@@ -22,12 +23,14 @@ public class WikipediaJsonTest {
 		WikipediaQuery query = gson.fromJson(json, WikipediaQuery.class);
 		assertNotNull("Parser returned null", query);
 		assertNotNull("Query in results shouldn't be null", query.getQuery());
-		assertNotNull("Map of pages shouldn't be null", query.getQuery()
-				.getPages());
+		assertNotNull("Map of pages shouldn't be null", query.getQuery().getPages());
 
-		assertEquals("Not exactly 1 page in result", query.getQuery()
-				.getPages().size(), 1);
+		assertEquals("Not exactly 1 page in result", 1, query.getQuery().getPages().size());
 
+		Page page = query.getQuery().getPages().values().iterator().next();
+		assertNotNull("Page is null", page);
+		assertEquals("Not exactly 174 langlinks", 174, page.getLanglinks().size());
+		assertEquals("First langlink lang is not ace", "ace", page.getLanglinks().get(0).getLang());
+		assertEquals("First langlink translation is not Albert Einstein", "Albert Einstein", page.getLanglinks().get(0).getTranslation());
 	}
-
 }
