dcbe3bbb5fb94dad1fb485ddcf4083bff64482f5
Kiraly Peter <peter.kiraly@kb.nl>
2014-06-04 11:20:14 +0200

    Re #1434 Refreshing SoundCloud test data.

--- corelib-web/src/test/java/eu/europeana/corelib/web/service/SoundCloudApiServiceTest.java
+++ corelib-web/src/test/java/eu/europeana/corelib/web/service/SoundCloudApiServiceTest.java
@@ -2,8 +2,12 @@ package eu.europeana.corelib.web.service;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,21 +22,52 @@ public class SoundCloudApiServiceTest {
 	@Resource
 	private SoundCloudApiService service;
 
+	private List<TrackData> pairs;
+
+	@Before
+	public void setup() {
+		pairs = new ArrayList<TrackData>();
+		pairs.add(new TrackData("dick-1952/geluid-diverse-elektrische", "150424305"));
+		pairs.add(new TrackData("dick-1952/geluid-van-een-vertrekkende", "137927336"));
+		pairs.add(new TrackData("classicsandjazz/01-hungarian-dance-no-5", "114360389"));
+	}
+
 	@Test
 	public void testResolvePath() {
-		Track track = service.resolvePath("dick-1952/interieur-treinstel-matr-36");
-		assertEquals("49487785", track.getId());
-		assertEquals("http://soundcloud.com/dick-1952/interieur-treinstel-matr-36", track.getPermalink_url());
-		assertEquals("49487785", track.getId());
-		assertEquals("dick-1952/interieur-treinstel-matr-36", track.getPath());
+		for (TrackData pair : pairs) {
+			Track track = service.resolvePath(pair.getPath());
+			assertEquals(pair.getPath(), track.getPath());
+			assertEquals(pair.getTrackId(), track.getId());
+			assertEquals("http://soundcloud.com/" + pair.getPath(), track.getPermalink_url());
+		}
 	}
 
 	@Test
 	public void testTrackInfo() {
-		Track track = service.getTrackInfo("49487785");
-		assertEquals("49487785", track.getId());
-		assertEquals("http://soundcloud.com/dick-1952/interieur-treinstel-matr-36", track.getPermalink_url());
-		assertEquals("49487785", track.getId());
-		assertEquals("dick-1952/interieur-treinstel-matr-36", track.getPath());
+		for (TrackData pair : pairs) {
+			Track track = service.getTrackInfo(pair.getTrackId());
+			assertEquals(pair.getPath(), track.getPath());
+			assertEquals(pair.getTrackId(), track.getId());
+			assertEquals("http://soundcloud.com/" + pair.getPath(), track.getPermalink_url());
+		}
+	}
+
+	private class TrackData {
+		private String path;
+		private String trackId;
+
+		public TrackData(String path, String trackId) {
+			super();
+			this.path = path;
+			this.trackId = trackId;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public String getTrackId() {
+			return trackId;
+		}
 	}
 }
