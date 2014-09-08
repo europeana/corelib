fcffc87e8a5d2b0b8d84868f42bb2d078ad7f75b
ymamakis <yorgos.mamakis@kb.nl>
2014-09-08 11:48:35 +0200

    Re #1671: Multithreaded support resolving concurrency issues

--- corelib-solr/src/main/java/eu/europeana/corelib/solr/server/impl/Neo4jServerImpl.java
+++ corelib-solr/src/main/java/eu/europeana/corelib/solr/server/impl/Neo4jServerImpl.java
@@ -6,14 +6,18 @@
 package eu.europeana.corelib.solr.server.impl;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.JsonNodeFactory;
@@ -53,14 +57,17 @@ public class Neo4jServerImpl implements Neo4jServer {
 	private String customPath;
 	private String serverPath;
 
-	private static final Relation edmIsNextInSequenceRelation = new Relation(RelType.EDM_ISNEXTINSEQUENCE.getRelType());
-	private static final Relation isFirstInSequenceRelation = new Relation(RelType.ISFIRSTINSEQUENCE.getRelType());
-	private static final Relation dctermsIsPartOfRelation = new Relation(RelType.DCTERMS_ISPARTOF.getRelType());
+	private static final Relation edmIsNextInSequenceRelation = new Relation(
+			RelType.EDM_ISNEXTINSEQUENCE.getRelType());
+	private static final Relation isFirstInSequenceRelation = new Relation(
+			RelType.ISFIRSTINSEQUENCE.getRelType());
+	private static final Relation dctermsIsPartOfRelation = new Relation(
+			RelType.DCTERMS_ISPARTOF.getRelType());
 
 	public Neo4jServerImpl(String serverPath, String index, String customPath) {
 		this.graphDb = new RestGraphDatabase(serverPath);
 		this.index = this.graphDb.getRestAPI().getIndex(index);
-		this.client = new HttpClient();
+		this.client = new HttpClient(new MultiThreadedHttpConnectionManager());
 		this.customPath = customPath;
 		this.serverPath = serverPath;
 	}
@@ -78,7 +85,8 @@ public class Neo4jServerImpl implements Neo4jServer {
 	public List<Node> getChildren(Node id, int offset, int limit) {
 
 		List<Node> children = new ArrayList<Node>();
-		RestTraversal traversal = (RestTraversal) graphDb.traversalDescription();
+		RestTraversal traversal = (RestTraversal) graphDb
+				.traversalDescription();
 
 		traversal.evaluator(Evaluators.excludeStartPosition());
 
@@ -109,7 +117,8 @@ public class Neo4jServerImpl implements Neo4jServer {
 
 	@Override
 	public Node getParent(Node id) {
-		List<Node> nodes = getRelatedNodes(id, 1, 0, Direction.OUTGOING, dctermsIsPartOfRelation);
+		List<Node> nodes = getRelatedNodes(id, 1, 0, Direction.OUTGOING,
+				dctermsIsPartOfRelation);
 		if (nodes.size() > 0) {
 			return nodes.get(0);
 		}
@@ -122,7 +131,8 @@ public class Neo4jServerImpl implements Neo4jServer {
 	}
 
 	private List<Node> getFollowingSiblings(Node id, int limit, int offset) {
-		return getRelatedNodes(id, limit, offset, Direction.INCOMING, edmIsNextInSequenceRelation);
+		return getRelatedNodes(id, limit, offset, Direction.INCOMING,
+				edmIsNextInSequenceRelation);
 	}
 
 	@Override
@@ -131,11 +141,12 @@ public class Neo4jServerImpl implements Neo4jServer {
 	}
 
 	private List<Node> getPreceedingSiblings(Node id, int limit, int offset) {
-		return getRelatedNodes(id, limit, offset, Direction.OUTGOING, edmIsNextInSequenceRelation);
+		return getRelatedNodes(id, limit, offset, Direction.OUTGOING,
+				edmIsNextInSequenceRelation);
 	}
 
-	private List<Node> getRelatedNodes(Node id, int limit, int offset, Direction direction,
-			Relation relType) {
+	private List<Node> getRelatedNodes(Node id, int limit, int offset,
+			Direction direction, Relation relType) {
 		List<Node> children = new ArrayList<Node>();
 		Transaction tx = graphDb.beginTx();
 		RestTraversal traversal = (RestTraversal) graphDb
@@ -173,78 +184,95 @@ public class Neo4jServerImpl implements Neo4jServer {
 	@Override
 	public long getChildrenCount(Node id) {
 
-		// start n = node(id) match (n)-[:HAS_PART]->(part) RETURN COUNT(part) as children
+		// start n = node(id) match (n)-[:HAS_PART]->(part) RETURN COUNT(part)
+		// as children
 		ObjectNode obj = JsonNodeFactory.instance.objectNode();
 		ArrayNode statements = JsonNodeFactory.instance.arrayNode();
 		obj.put("statements", statements);
 		ObjectNode statement = JsonNodeFactory.instance.objectNode();
-		statement.put("statement",
-			"start n = node:edmsearch2(rdf_about={from}) match (n)-[:`dcterms:hasPart`]->(part) RETURN COUNT(part) as children");
+		statement
+				.put("statement",
+						"start n = node:edmsearch2(rdf_about={from}) match (n)-[:`dcterms:hasPart`]->(part) RETURN COUNT(part) as children");
 		ObjectNode parameters = statement.with("parameters");
 		statements.add(statement);
 		parameters.put("from", (String) id.getProperty("rdf:about"));
-		try{
+		PostMethod httpMethod = new PostMethod(serverPath
+				+ "transaction/commit");
+		try {
 			String str = new ObjectMapper().writeValueAsString(obj);
-			PostMethod httpMethod = new PostMethod(serverPath + "transaction/commit");
+			
 			httpMethod.setRequestBody(str);
 			httpMethod.setRequestHeader("content-type", "application/json");
 			client.executeMethod(httpMethod);
 
 			CustomResponse cr = new ObjectMapper().readValue(
 					httpMethod.getResponseBodyAsStream(), CustomResponse.class);
-			if (cr.getResults() != null && cr.getResults().size() > 0
+			if (cr.getResults() != null
+					&& cr.getResults().size() > 0
 					&& cr.getResults().get(0) != null
-					&& cr.getResults().get(0).getData()!=null
+					&& cr.getResults().get(0).getData() != null
 					&& cr.getResults().get(0).getData().get(0) != null
 					&& cr.getResults().get(0).getData().get(0).get("row") != null
-					&& cr.getResults().get(0).getData().get(0).get("row").size() > 0)
-				return Long.parseLong(cr.getResults().get(0).getData().get(0).get("row").get(0));
-		} catch (Exception e){
+					&& cr.getResults().get(0).getData().get(0).get("row")
+							.size() > 0)
+				return Long.parseLong(cr.getResults().get(0).getData().get(0)
+						.get("row").get(0));
+		} catch (Exception e) {
 			log.error("error: " + e.getLocalizedMessage());
 			e.printStackTrace();
+		} finally {
+			httpMethod.releaseConnection();
 		}
 
 		return 0;
 	}
 
 	@Override
-	public long getNodeIndex(Node node){
-		GetMethod method = new GetMethod(customPath + "/europeana/hierarchycount/nodeId/" + node.getId());
+	public long getNodeIndex(Node node) {
+		GetMethod method = new GetMethod(customPath
+				+ "/europeana/hierarchycount/nodeId/" + node.getId());
 		try {
-			log.info("path: " + method.getPath());
+			// log.info("path: " + method.getPath());
 			client.executeMethod(method);
-			String respBody = method.getResponseBodyAsString();
-			IndexObject obj = new ObjectMapper().readValue(respBody, IndexObject.class);
+
+			IndexObject obj = new ObjectMapper().readValue(
+					method.getResponseBodyAsStream(), IndexObject.class);
 			return obj.getLength();
 		} catch (HttpException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			method.releaseConnection();
 		}
-
 		return 0;
 	}
 
 	@Override
-	public Hierarchy  getInitialStruct(String id) {
-		GetMethod method = new GetMethod(customPath + "/initial/startup/nodeId/" + StringUtils.replace(id, "/", "%2F"));
+	public Hierarchy getInitialStruct(String id) {
+		GetMethod method = new GetMethod(customPath
+				+ "/initial/startup/nodeId/"
+				+ StringUtils.replace(id, "/", "%2F"));
 		log.info("path: " + method.getPath());
 		try {
 			client.executeMethod(method);
-			String respBody = method.getResponseBodyAsString();
+
 			ObjectMapper mapper = new ObjectMapper();
-			Hierarchy obj = mapper.readValue(respBody, Hierarchy.class);
+			Hierarchy obj = mapper.readValue(method.getResponseBodyAsStream(),
+					Hierarchy.class);
 			return obj;
 		} catch (HttpException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			method.releaseConnection();
 		}
 		return null;
 	}
 
 	@Override
-	public String getCustomPath(){
+	public String getCustomPath() {
 		return customPath;
 	}
 }
