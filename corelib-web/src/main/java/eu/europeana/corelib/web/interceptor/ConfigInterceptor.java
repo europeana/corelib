ed54146cfe5ef08d4f722bf3805f289b1c38401e
Andy MacLean <andyjmaclean@gmail.com>
2015-02-02 16:08:59 +0100

    Undo test request info behaviour behind apache

--- corelib-web/src/main/java/eu/europeana/corelib/web/interceptor/ConfigInterceptor.java
+++ corelib-web/src/main/java/eu/europeana/corelib/web/interceptor/ConfigInterceptor.java
@@ -75,7 +75,7 @@ public class ConfigInterceptor extends HandlerInterceptorAdapter {
 		if (modelAndView != null && !modelAndView.getViewName().startsWith("redirect:")
 				&& modelAndView.getModel().containsKey(PageData.PARAM_MODEL)) {
 			PageData model = (PageData) modelAndView.getModel().get(PageData.PARAM_MODEL);
-			StringBuilder currentUrl = new StringBuilder(config.getPortalServer()+"/"+request.getPathInfo());
+			StringBuilder currentUrl = new StringBuilder(request.getRequestURI());
 			if (request.getQueryString() != null) {
 				currentUrl.append("?").append(request.getQueryString());
 			}
