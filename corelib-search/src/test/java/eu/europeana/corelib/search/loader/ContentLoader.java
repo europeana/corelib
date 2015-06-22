32cfecb68af36ca1286f0d3ebf5ab4f5933cdfd6
Willem-Jan Boogerd <wjboogerd@users.noreply.github.com>
2015-06-22 14:53:20 +0200

    - Reversed corelib-solr-context.xml to prevent problems with other projects at this point

--- corelib-search/src/test/java/eu/europeana/corelib/search/loader/ContentLoader.java
+++ corelib-search/src/test/java/eu/europeana/corelib/search/loader/ContentLoader.java
@@ -274,7 +274,7 @@ public class ContentLoader {
      */
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("/corelib-search-context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("/corelib-solr-context.xml");
 
         SolrServer solrServer = context.getBean("corelib_solr_solrEmbedded", SolrServer.class);
         EdmMongoServer mongoDBServer = context.getBean("corelib_solr_mongoServer", EdmMongoServer.class);
