2315a91e5d8405731a650728d14d729b11af9fc0
ymamakis <yorgos.mamakis@europeana.eu>
2015-12-07 13:46:54 +0100

    Made postgres configurable

--- corelib-web/src/main/java/eu/europeana/corelib/web/context/VcapPropertyLoaderListener.java
+++ corelib-web/src/main/java/eu/europeana/corelib/web/context/VcapPropertyLoaderListener.java
@@ -93,6 +93,11 @@ public class VcapPropertyLoaderListener extends VcapApplicationListener {
   private final static String PORTALSERVER = "portal_server";
   private final static String PORTALCANONICALURL = "portal_server_canonical";
   private final static String POSTGRES = "postgres";
+  private final static String SWIFT_AUTHENTICATION_URL="vcap.services.swift-sitemap.credentials.authentication_uri";
+  private final static String SWIFT_AUTHENTICATION_AV_ZONE="vcap.services.swift-sitemap.credentials.availability_zone";
+  private final static String SWIFT_AUTHENTICATION_TENANT_NAME="vcap.services.swift-sitemap.credentials.tenant_name";
+  private final static String SWIFT_AUTHENTICATION_USER_NAME="vcap.services.swift-sitemap.credentials.user_name";
+  private final static String SWIFT_AUTHENTICATION_PASSWORD="vcap.services.swift-sitemap.credentials.password";
 
   private static StandardServletEnvironment env = new StandardServletEnvironment();
 
@@ -112,16 +117,16 @@ public class VcapPropertyLoaderListener extends VcapApplicationListener {
     try {
       props.load(new FileInputStream(europeanaProperties));
 
-      final String[] swiftProp = new String[] { "authUrl", "userName", "password", "containerName", "regionName", "tenantName"};
+
 
 
 
       // PostgreSQL db, username, password, host
       if (env.getProperty(POSTGRESHOST) != null) {
-        props.setProperty("postgres.db", env.getProperty(POSTGRESDB+env.getProperty(POSTGRES)+CREDENTIALS_DB));
-        props.setProperty("postgres.username", env.getProperty(POSTGRESUSERNAME+env.getProperty(POSTGRES)+CREDENTIALS_USER));
-        props.setProperty("postgres.password", env.getProperty(POSTGRESPASSWORD)+env.getProperty(POSTGRES)+CREDENTIALS_PASSWORD);
-        props.setProperty("postgres.host", env.getProperty(POSTGRESHOST)+env.getProperty(POSTGRES)+CREDENTIALS_HOST);
+        props.setProperty("postgres.db", env.getProperty(POSTGRESDB));
+        props.setProperty("postgres.username", env.getProperty(POSTGRESUSERNAME));
+        props.setProperty("postgres.password", env.getProperty(POSTGRESPASSWORD));
+        props.setProperty("postgres.host", env.getProperty(POSTGRESHOST));
       }
 
       // MongoDB username, password, host, port
