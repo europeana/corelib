616a09ebf5799befe9810194a6f625b9206701d0
bramfoo <bram.lohman@europeana.eu>
2016-02-02 16:28:54 +0100

    Added missing import; removed missing SOUND enum

--- corelib-search/src/main/java/eu/europeana/corelib/search/service/impl/FacetLabelExtractor.java
+++ corelib-search/src/main/java/eu/europeana/corelib/search/service/impl/FacetLabelExtractor.java
@@ -1,6 +1,7 @@
 package eu.europeana.corelib.search.service.impl;
 
 import eu.europeana.corelib.definitions.model.facets.inverseLogic.*;
+import eu.europeana.crf_faketags.extractor.MediaTypeEncoding;
 
 public class FacetLabelExtractor {
 
@@ -29,13 +30,14 @@ public class FacetLabelExtractor {
                 }
                 label = ImagePropertyExtractor.getSize(tag);
                 return label;
-            case SOUND:
-                label = SoundPropertyExtractor.getDuration(tag);
-                if(!label.equals("")) {
-                    return label;
-                }
-                label = SoundPropertyExtractor.getQuality(tag);
-                return label;
+// FIXME: no such enum in crf-fake-tags                
+//            case SOUND:
+//                label = SoundPropertyExtractor.getDuration(tag);
+//                if(!label.equals("")) {
+//                    return label;
+//                }
+//                label = SoundPropertyExtractor.getQuality(tag);
+//                return label;
             case VIDEO:
                 label = VideoPropertyExtractor.getDuration(tag);
                 if(!label.equals("")) {
