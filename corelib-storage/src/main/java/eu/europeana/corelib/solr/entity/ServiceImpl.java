cd158eafe0128d08a8e6932d0bceb503470c8e2f
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-05-02 14:16:06 +0200

    better solution: use Jackson v.2 @JsonIgnore in ServiceImpl

--- corelib-storage/src/main/java/eu/europeana/corelib/solr/entity/ServiceImpl.java
+++ corelib-storage/src/main/java/eu/europeana/corelib/solr/entity/ServiceImpl.java
@@ -1,5 +1,6 @@
 package eu.europeana.corelib.solr.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
 import eu.europeana.corelib.definitions.edm.entity.Service;
@@ -13,10 +14,10 @@ public class ServiceImpl extends AbstractEdmEntityImpl implements Service {
     private String[] dctermsConformsTo;
     private String[] doapImplements;
 
-
+    @JsonIgnore
     @Override
     public ObjectId getId() {
-        return null;
+        return this.id;
     }
 
     @Override
