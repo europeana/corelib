aad5e91194bc4d6b1b07d7b6439c702be4520f3b
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-22 14:56:42 +0200

    updated license.md and removed all license headers from source files

--- corelib-db/src/main/java/eu/europeana/corelib/db/internal/service/impl/OAuth2TokenRefreshServiceImpl.java
+++ corelib-db/src/main/java/eu/europeana/corelib/db/internal/service/impl/OAuth2TokenRefreshServiceImpl.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2007-2012 The Europeana Foundation
- *
- *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved 
- *  by the European Commission;
- *  You may not use this work except in compliance with the Licence.
- *  
- *  You may obtain a copy of the Licence at:
- *  http://joinup.ec.europa.eu/software/page/eupl
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under 
- *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of 
- *  any kind, either express or implied.
- *  See the Licence for the specific language governing permissions and limitations under 
- *  the Licence.
- */package eu.europeana.corelib.db.internal.service.impl;
+package eu.europeana.corelib.db.internal.service.impl;
 
 import eu.europeana.corelib.db.entity.nosql.RefreshToken;
 import eu.europeana.corelib.db.internal.service.OAuth2RefreshTokenService;
--- corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/web/BreadCrumb.java
+++ corelib-definitions/src/main/java/eu/europeana/corelib/definitions/model/web/BreadCrumb.java
@@ -1,24 +1,3 @@
-/*
- * Copyright 2007 EDL FOUNDATION
- *
- * Licensed under the EUPL, Version 1.0 or - as soon they
- * will be approved by the European Commission - subsequent
- * versions of the EUPL (the "License");
- * you may not use this work except in compliance with the
- * License.
- * You may obtain a copy of the License at:
- *
- * http://ec.europa.eu/idabc/eupl
- *
- * Unless required by applicable law or agreed to in
- * writing, software distributed under the License is
- * distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package eu.europeana.corelib.definitions.model.web;
 
 import org.apache.commons.lang.StringUtils;
