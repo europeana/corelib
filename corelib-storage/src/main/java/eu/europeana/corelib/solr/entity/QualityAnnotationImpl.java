278c8f61d249623258850ac394002254c488570f
stzanakis <simon.tzanakis@europeana.eu>
2019-07-02 15:25:25 +0200

    Fix indexed fields in QualityAnnotationImpl

--- corelib-storage/src/main/java/eu/europeana/corelib/solr/entity/QualityAnnotationImpl.java
+++ corelib-storage/src/main/java/eu/europeana/corelib/solr/entity/QualityAnnotationImpl.java
@@ -1,18 +1,51 @@
 package eu.europeana.corelib.solr.entity;
 
+import eu.europeana.corelib.definitions.edm.entity.AbstractEdmEntity;
 import eu.europeana.corelib.definitions.edm.entity.QualityAnnotation;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Indexed;
 
 /**
+ * Contains Quality annotation tier information.
+ * <p>This class does not extend from {@link AbstractEdmEntityImpl} but rather implements the
+ * {@link AbstractEdmEntity} to avoid the about field being indexed.<p/>
+ *
  * @author Simon Tzanakis (Simon.Tzanakis@europeana.eu)
  * @since 2019-06-13
  */
 @Entity("QualityAnnotation")
-public class QualityAnnotationImpl extends AbstractEdmEntityImpl implements QualityAnnotation {
+public class QualityAnnotationImpl implements AbstractEdmEntity, QualityAnnotation {
+
+  @Id
+  protected ObjectId id = new ObjectId();
+  protected String about;
 
   private String created;
   private String[] target;
+  @Indexed
   private String body;
+
+  @Override
+  public ObjectId getId() {
+    return this.id;
+  }
+
+  @Override
+  public void setId(ObjectId id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getAbout() {
+    return this.about;
+  }
+
+  @Override
+  public void setAbout(String about) {
+    this.about = about;
+  }
 
   @Override
   public String getCreated() {
