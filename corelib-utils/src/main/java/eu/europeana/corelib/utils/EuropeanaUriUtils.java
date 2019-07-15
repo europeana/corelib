15d43e05b872ab899300663b8a80a9d4074bb75c
srishti <srishti.singh@europeana.eu>
2019-07-15 12:01:43 +0200

    Detecting valid URIs for the rdf/xml output

--- corelib-utils/src/main/java/eu/europeana/corelib/utils/EuropeanaUriUtils.java
+++ corelib-utils/src/main/java/eu/europeana/corelib/utils/EuropeanaUriUtils.java
@@ -1,5 +1,17 @@
 package eu.europeana.corelib.utils;
 
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -18,6 +30,12 @@ import org.apache.commons.lang3.StringUtils;
 public class EuropeanaUriUtils {
 
 	private final static String REPLACEMENT = "_";
+
+	private static Charset UTF8  = Charset.forName("UTF-8");
+	private static String  P_STR = "^([a-zA-Z][a-zA-Z+-.]*):.*$";
+
+	public  static Pattern            PATTERN = Pattern.compile(P_STR);
+	public  static Collection<String> SCHEMES = loadSchemes();
 
 	private EuropeanaUriUtils() {
 
@@ -80,11 +98,45 @@ public class EuropeanaUriUtils {
 		}
 		return value;
 	}
-	
+
+
 	public static boolean isUri(String str) {
-	    return StringUtils.startsWith(str, "http://")
-	           || StringUtils.startsWith(str, "https://")
-	           || StringUtils.startsWith(str, "urn:")
-	           || StringUtils.startsWith(str, "#");
+		return ( isRelativeIRI(str) || isAbsoluteIRI(str) );
 	}
+
+	//Loads the schemas from the  iri.schemes.cfg in resource folder
+
+	private static Collection<String> loadSchemes()
+	{
+		Collection<String> schemes = new TreeSet<String>();
+		try
+		{
+			URL url = EuropeanaUriUtils.class.getResource("/resources/iri.schemes.cfg");
+			CSVParser parser = CSVParser.parse(url, UTF8, CSVFormat.EXCEL);
+			int i = 0;
+			for ( CSVRecord record : parser.getRecords() )
+			{
+				if ( i++ == 0 ) { continue; }
+				schemes.add(record.get(0));
+			}
+		}
+		catch (IOException e) {}
+
+		return schemes;
+	}
+
+	// will check is it's a absolute or relative URI
+
+	public static boolean isAbsoluteIRI(String iri)
+	{
+		Matcher m = PATTERN.matcher(iri);
+		return ( m.find() ? SCHEMES.contains(m.group(1)) : false );
+	}
+
+	public static boolean isRelativeIRI(String iri)
+	{
+		return ( iri.startsWith("/")   || iri.startsWith("#")
+				|| iri.startsWith("../") || iri.startsWith("./") );
+	}
+
 }
