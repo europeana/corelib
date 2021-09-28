c14c9744b9068a18878c2fee31bc18e98437bdcb
Luthien-in-edhil <luthien@parendili.org>
2021-09-28 10:46:45 +0200

    triggering Jenkins build

--- corelib-definitions/src/main/java/eu/europeana/corelib/definitions/edm/beans/RichBean.java
+++ corelib-definitions/src/main/java/eu/europeana/corelib/definitions/edm/beans/RichBean.java
@@ -25,7 +25,6 @@ public interface RichBean extends ApiBean {
     Map<String, List<String>> getDcSubjectLangAware();
 
     /**
-     *
      * @return foaf_organization (organizations)
      */
     String[] getOrganizations();
